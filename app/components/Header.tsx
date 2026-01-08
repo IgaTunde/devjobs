aedbbf003565fe19ca18b68e9f85cc1359d680b1
Iga Tunde <igatunde06@gmail.com>
2026-01-08 12:53:03 +0100

    fixed header backgroudn styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,30 +2,28 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="w-full">
-      <div className="relative w-full h-[136px] md:h-40 lg:h-40">
-        {/* Desktop */}
+    <header className="w-full h-[136px] md:h-40 lg:h-40 relative bg-cover bg-left bg-no-repeat bg-[url('/assets/mobile/bg-pattern-header.svg')] md:bg-[url('/assets/tablet/bg-pattern-header.svg')] lg:bg-[url('/assets/desktop/bg-pattern-header.svg')]">
+      <div className="flex flex-row justify-between items-center h-full px-4 sm:px-6 lg:px-8">
         <Image
-          src="/assets/desktop/bg-pattern-header.svg"
-          alt="Hero"
-          fill
-          className="hidden lg:block object-contain"
-          priority
+          src="/assets/desktop/logo.svg"
+          alt="Logo"
+          width={30}
+          height={32}
         />
-        {/* Tablet */}
-        <Image
-          src="/assets/tablet/bg-pattern-header.svg"
-          alt="Hero"
-          fill
-          className="hidden md:block lg:hidden object-cover"
-        />
-        {/* Mobile */}
-        <Image
-          src="/assets/mobile/bg-pattern-header.svg"
-          alt="Hero"
-          fill
-          className="block md:hidden object-cover"
-        />
+        <div className="flex flex-row gap-4 items-center">
+          <Image
+            src="/assets/desktop/icon-sun.svg"
+            alt="Sun"
+            width={20}
+            height={20}
+          />
+          <Image
+            src="/assets/desktop/icon-moon.svg"
+            alt="Moon"
+            width={12}
+            height={12}
+          />
+        </div>
       </div>
     </header>
   );
